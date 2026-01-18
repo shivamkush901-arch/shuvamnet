3753f0ae500b5d0e26a3a1660b5be6d516cadc92
shivamkush901-arch <shivamkush901@gmail.com>
2026-01-18 12:31:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function sayHello() {
+    document.querySelector("p").innerText =
+    "Maine apni pehli website khud banayi hai 💪";
+}
 
+function sendMessage(event) {
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+
+    document.getElementById("result").innerText =
+    "Thank you " + name + ", your message has been sent ✅";
+
+    // form clear
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+}
